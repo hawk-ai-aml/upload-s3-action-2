d0607a09dd227f2e9594f0ceab6a1acde4c8b9bb
luca <luca.ricchi@hawk.ai>
2023-09-22 14:56:46 +0200

    try with private ACL

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function run() {
       );
       const params = {
         Bucket: BUCKET,
-        ACL: 'public-read',
+        ACL: 'private',
         Body: fileStream,
         Key: bucketPath,
         ContentType: lookup(p.path) || 'text/plain',
